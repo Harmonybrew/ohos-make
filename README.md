494362c90dbabeb635cf2301bb0721c2b46ee1e1
hqzing <hqzing@outlook.com>
2026-03-19 17:42:03 +0800

    第一个版本

--- README.md
+++ README.md
@@ -1,1 +1,77 @@
 # ohos-make
+
+本项目为 OpenHarmony 平台编译了 make，并发布预构建包。
+
+## 获取预构建包
+
+前往 [release 页面](https://github.com/Harmonybrew/ohos-make/releases) 获取。
+
+## 用法
+**1\. 在鸿蒙 PC 中使用**
+
+在 HiShell 中用 curl 下载这个软件包，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+cd ~
+curl -fLO https://github.com/Harmonybrew/ohos-make/releases/download/4.4.1/make-4.4.1-ohos-arm64.tar.gz
+tar -zxf make-4.4.1-ohos-arm64.tar.gz
+export PATH=~/make-4.4.1-ohos-arm64/bin:$PATH
+
+# 现在可以使用 make 命令了
+```
+
+**2\. 在鸿蒙开发板中使用**
+
+用 hdc 把它推到设备上，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+hdc file send make-4.4.1-ohos-arm64.tar.gz /data
+hdc shell
+
+cd /data
+tar -zxf make-4.4.1-ohos-arm64.tar.gz
+export PATH=/data/make-4.4.1-ohos-arm64/bin:$PATH
+
+# 现在可以使用 make 命令了
+```
+
+**3\. 在 [鸿蒙容器](https://github.com/hqzing/dockerharmony) 中使用**
+
+在容器中用 curl 下载这个软件包，然后以“解压 + 配 PATH” 的方式使用。
+
+示例：
+```sh
+cd /opt
+curl -fLO https://github.com/Harmonybrew/ohos-make/releases/download/4.4.1/make-4.4.1-ohos-arm64.tar.gz
+tar -zxf make-4.4.1-ohos-arm64.tar.gz
+export PATH=/opt/make-4.4.1-ohos-arm64/bin:$PATH
+
+# 现在可以使用 make 命令了
+```
+
+## 从源码构建
+
+**1\. 手动构建**
+
+需要用一台 Linux x64 服务器来运行项目里的 build.sh，以实现 make 的交叉编译。
+
+这里以 Ubuntu 24.04 x64 作为示例：
+```sh
+sudo apt update && sudo apt install -y build-essential unzip
+git clone https://github.com/Harmonybrew/ohos-make.git
+cd ohos-make
+./build.sh
+```
+
+**2\. 使用流水线构建**
+
+如果你熟悉 GitHub Actions，你可以直接复用项目内的工作流配置，使用 GitHub 的流水线来完成构建。
+
+这种情况下，你使用的是 GitHub 提供的构建机，不需要自己准备构建环境。
+
+只需要这么做，你就可以进行你的个人构建：
+1. Fork 本项目，生成个人仓
+2. 在个人仓的“Actions”菜单里面启用工作流
+3. 在个人仓提交代码或发版本，触发流水线运行
---
+++ build.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+set -e
+
+# 当前工作目录。拼接绝对路径的时候需要用到这个值。
+WORKDIR=$(pwd)
+
+# 如果存在旧的目录和文件，就清理掉
+rm -rf *.tar.gz \
+    ohos-sdk \
+    daily_build.log \
+    manifest_tag.xml \
+    make-4.4.1 \
+    make-4.4.1-ohos-arm64
+
+# 准备 ohos-sdk
+curl -fL -o ohos-sdk-full_6.1-Release.tar.gz https://cidownload.openharmony.cn/version/Master_Version/OpenHarmony_6.1.0.31/20260311_020435/version-Master_Version-OpenHarmony_6.1.0.31-20260311_020435-ohos-sdk-full_6.1-Release.tar.gz
+tar -zxf ohos-sdk-full_6.1-Release.tar.gz
+cd ohos-sdk/linux
+unzip -q native-*.zip
+unzip -q toolchains-*.zip
+cd ../..
+
+# 设置交叉编译所需的环境变量
+export LLVM_BIN=$WORKDIR/ohos-sdk/linux/native/llvm/bin
+export CC=$LLVM_BIN/aarch64-unknown-linux-ohos-clang
+export CXX=$LLVM_BIN/aarch64-unknown-linux-ohos-clang++
+export LD=$LLVM_BIN/ld.lld
+export AR=$LLVM_BIN/llvm-ar
+export AS=$LLVM_BIN/llvm-as
+export NM=$LLVM_BIN/llvm-nm
+export OBJCOPY=$LLVM_BIN/llvm-objcopy
+export OBJDUMP=$LLVM_BIN/llvm-objdump
+export RANLIB=$LLVM_BIN/llvm-ranlib
+export STRIP=$LLVM_BIN/llvm-strip
+
+# 编译 make
+curl -fLO https://ftp.gnu.org/gnu/make/make-4.4.1.tar.gz
+tar -zxf make-4.4.1.tar.gz
+cd make-4.4.1
+./configure --prefix=$WORKDIR/make-4.4.1-ohos-arm64 --host=aarch64-linux
+sed -i '/#define MAKE_CXX/c #define MAKE_CXX "c++"' src/config.h
+make -j$(nproc)
+make install
+cd ..
+
+# 添加代码签名
+cd $WORKDIR/make-4.4.1-ohos-arm64
+find . -type f \( -perm -0111 -o -name "*.so*" \) | while read FILE; do
+    if file -b "$FILE" | grep -iqE "elf|sharedlib|ELF|shared object"; then
+        echo "Signing binary file $FILE"
+        ORIG_PERM=$(stat -c %a "$FILE")
+        $WORKDIR/ohos-sdk/linux/toolchains/lib/binary-sign-tool sign -inFile "$FILE" -outFile "$FILE" -selfSign 1
+        chmod "$ORIG_PERM" "$FILE"
+    fi
+done
+cd $WORKDIR
+
+# 履行开源义务，将 license 随制品一起发布
+cp make-4.4.1/COPYING make-4.4.1-ohos-arm64/
+
+# 打包最终产物
+tar -zcf make-4.4.1-ohos-arm64.tar.gz make-4.4.1-ohos-arm64
